c756385b207e1287a1d1b381b21f16f43484001f
shiyifeng <shiyifeng27@163.com>
2017-11-16 09:22:58 +0800

    本地测试冲突

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("success");
-        ahahaha
+        System.out.println("123");
     }
 
 }
